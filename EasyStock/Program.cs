aaf43ab78b2f4c7666508d24b4b322e72a82ea19
nryan <ryan.nedjari@ynov.com>
2025-05-22 17:16:15 +0200

    Initial commit - Full EasyStock project

---
+++ EasyStock/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+string connectionString = "Server=localhost;Database=easystock;User ID=root;Password=root;";
+
+using var connection = new MySqlConnection(connectionString);
+try
+{
+    connection.Open();
+    Console.WriteLine("Connexion réussie !");
+    
+    var command = new MySqlCommand("SELECT * FROM product", connection);
+    using var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"Produit : {reader["NAME"]}, Stock : {reader["STOCK"]}");
+    }
+    if (reader.HasRows)
+    {
+        Console.WriteLine("Des produits ont été trouvés.");
+    }
+    else
+    {
+        Console.WriteLine("Aucun produit trouvé.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erreur : " + ex.Message);
+}
